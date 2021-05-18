db14cb679a4ca3008281e69e5b79fb0d76d49f69
uncoyote <jeker.git@outlook.de>
2021-05-18 16:23:18 +0200

    permission trafo sql

---
+++ api_webgisclient/sql2json/permission/groups.sql
@@ -0,0 +1,32 @@
+with 
+
+ugroup as (
+	select
+		sg.id as group_id,
+		si.identifier as group_identifier 
+	from 
+		simi.simiiam_group sg 
+	inner join 
+		simi.simiiam_identity si on sg.id = si.id 
+),
+
+group_roles as (
+	select 
+		group_id,
+		jsonb_agg(name) as role_arr
+	from 
+		simi.simiiam_role_group_link rg
+	inner join
+		simi.simiiam_role r on rg.role_id = r.id
+	group by 
+		group_id	
+)
+
+select
+	json_build_object('name', group_identifier, 'roles', role_arr) as group_json 
+from 
+	ugroup g
+left outer join 
+	group_roles r on g.group_id = r.group_id
+;
+	

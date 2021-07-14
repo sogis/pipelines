442f52081943bd3dae49cda98f9024d6a2f9715d
uncoyote <jeker.git@outlook.de>
2021-07-14 16:58:58 +0200

    merged trafo files for wms services into existing folders

--- api_webgisclient/sql2json/qgis-server-wfs/tojson.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-path="$(pwd)/../../../.gitignored/pipe_bin/sql2json/sql2json.jar -c jdbc:postgresql://localhost:5433/simi -u postgres -p postgres -t $(pwd)/template.json -o $(pwd)/../../../.gitignored/pipe_data/qgs/qgs_wfs.json -s https://github.com/simi-so/json2qgs/raw/master/schemas/sogis-wfs-qgs-content.json"
-#echo $path
-
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=info -jar $path

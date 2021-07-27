b5c3d777be6264df486eaaac5d879933ccefe25c
uncoyote <jeker.git@outlook.de>
2021-07-27 14:50:16 +0200

    finished less hacky view for qgs pg_conf

--- api_webgisclient/sql2json/views/trafo_wms_geotable_v.sql
+++ api_webgisclient/sql2json/views/trafo_wms_geotable_v.sql
@@ -5,26 +5,10 @@ DROP VIEW IF EXISTS simi.trafo_wms_geotable_v;
  */
 CREATE VIEW simi.trafo_wms_geotable_v AS 
 
-WITH 
-
-pgconf_name_db_map AS (
-  SELECT 
-    * 
-  FROM (
-    VALUES 
-      ('Geo DB alt', 'name1'),
-      ('DB Pub', 'name2'), 
-      ('DB Edit', 'name3'),
-      ('Oereb DB', 'name4')
-  ) 
-  AS t (db_name, pg_conf_name)
-)
-
-
 SELECT 
   tbl.id AS  table_id,
   jsonb_build_object(
-    'dbconnection', COALESCE(pg_conf_name, '!!ERROR DB Name mismatch in cte pgconf_name_db_map'),
+    'dbconnection', split_part(db.db_service_url, 'service=', 2),
     'schema', schema_name,
     'table', table_name,
     'unique_key', id_field_name,
@@ -38,8 +22,6 @@ JOIN
   simi.simidata_data_theme dt ON tbl.data_theme_id = dt.id 
 JOIN 
   simi.simidata_postgres_db db ON dt.postgres_db_id = db.id 
-LEFT JOIN
-  pgconf_name_db_map m ON db.db_name = m.db_name
 WHERE
     geo_field_name IS NOT NULL
   AND

5ba8f4db2b2807cb145d974931c300b346354977
uncoyote <jeker.git@outlook.de>
2021-08-04 16:56:48 +0200

    refactored permissions to generic dsv-groups

--- api_webgisclient/sql2json/templates/services-unified-permissions/role_permissions.sql
+++ api_webgisclient/sql2json/templates/services-unified-permissions/role_permissions.sql
@@ -1,417 +1,228 @@
-
-/* Gibt die Permissions pro Rolle für Dataproducts und Reports aus.
+/* Gibt die Permissions pro Rolle für DSV und DSV-Gruppen aus.
  * 
- * Die resultierende Permission für ein Facadelayer / eine Layergroup
- * wird aus den Kindern abgeleitet.
+ * Die resultierende Permission für DSV-Gruppen wird aus den DSV abgeleitet.
  * Damit eine Gruppierung für eine Rolle berechtigt ist, müssen alle
- * Kinder der entsprechenden Rolle oder der speziellen Rolle "public"
+ * DSV der entsprechenden privaten Rolle oder der speziellen Rolle "public"
  * zugewiesen sein.
  * 
- * In den CTEs mit "full outer join" werden jeweils die Eigenschaften
- * der Gruppe bezüglich der entsprechenden privaten Rolle und der Rolle
- * "public" vereinigt. Aus den beiden Informationen wird berechnet,
- * welche Berechtigung für die Rolle und Ebenen-Gruppe resultiert.
+ * Falls ein oder mehrere DSV einer DSV-Gruppe eine private Rolle zugewiesen
+ * haben, wird "public" für die Gruppe unterdrückt (nicht ausgegeben).
+ * Dies auch, falls allen DSV die Rolle public zugewiesen ist.
+ * Sollte nicht stören, da für die Gruppierungen sowieso nur Leseberechtigungen
+ * von public oder "private" gefragt ist.
  * 
- * Lösungsweg für fl_private_perm, fl_public_perm beschreiben
- * Fall mit layergruppe = '5877dcac-052a-4356-859c-2e3a4f28abb5' untersuchen
+ * Debugging aum einfachsten mit der CTE "role_perm". In dieser sind die entscheidenden
+ * Count-Spalten extra enthalten, obwohl für die folgenden CTE nicht relevant.
+ * 
  * */
 WITH
 
-dsv_perm_base AS (
-  SELECT 
-    level_ AS perm_level,
-    p.data_set_view_id AS dsv_id,
-    role_id,
-    (r."name" = 'public') AS is_public_role
-  FROM 
-    simi.simidata_data_set_view d
-  JOIN
-    simi.simiiam_permission p ON d.id = p.data_set_view_id 
-  JOIN
-    simi.simiiam_role r ON p.role_id = r.id  
+role_base AS (
+  SELECT
+    id AS role_id,
+    "name" AS role_name,
+    CASE
+      WHEN "name" = 'public' THEN TRUE
+      ELSE FALSE
+    END AS is_public_role
+  FROM
+    simi.simi.simiiam_role 
 ),
 
-dsv_public_perm AS (
+dsv_dsv AS ( -- Damit alles gleich gemacht werden kann: Dsv bildet einer Gruppe mit sich selbst
   SELECT 
-    *
+    id AS res_id,
+    id AS dsv_id
   FROM 
-    dsv_perm_base
+    simi.simidata_data_set_view 
+),
+
+fl_dsv AS (
+  SELECT 
+    facade_layer_id AS res_id,
+    data_set_view_id AS dsv_id
+  FROM 
+    simi.simiproduct_properties_in_facade pif
+),
+
+sa_dsv AS (
+  SELECT res_id, dsv_id FROM dsv_dsv
+  UNION ALL
+  SELECT res_id, dsv_id FROM fl_dsv
+),
+
+pl_dsv AS (
+  SELECT 
+    pil.product_list_id AS res_id,
+    dsv_id
+  FROM 
+    simi.simiproduct_properties_in_list pil
+  JOIN
+    sa_dsv sa ON pil.single_actor_id = sa.res_id
+),
+
+dep_dsv AS ( -- Reports und Feature-Info Konfigurationen, mit Datenbeziehung auf DSV
+  SELECT
+    r.dependency_id AS res_id,
+    r.data_set_view_id AS dsv_id
+  FROM 
+    simi.simi.simiextended_relation r
+  JOIN
+    simi.simi.simiextended_dependency d ON r.dependency_id = d.id
+  WHERE
+      d.dtype IN ('simiExtended_Report', 'simiExtended_FeatureInfo')
+    AND
+      r.relation_type = '2_data'
+),
+
+resgroups_union AS (
+  SELECT res_id, dsv_id FROM dsv_dsv
+  UNION ALL 
+  SELECT res_id, dsv_id FROM fl_dsv
+  UNION ALL 
+  SELECT res_id, dsv_id FROM pl_dsv
+  UNION ALL 
+  SELECT res_id, dsv_id FROM dep_dsv
+),
+
+res_dsv_total_count AS (
+  SELECT 
+    res_id,
+    count(*) AS dsv_count_per_res
+  FROM
+    resgroups_union    
+  GROUP BY 
+    res_id
+),
+
+res_role_perm AS (
+  SELECT 
+    res_id,
+    min(level_) AS perm_level,
+    count(*) AS dsv_count_per_res_role,
+    role_id
+  FROM
+    resgroups_union r
+  JOIN
+    simi.simiiam_permission p ON r.dsv_id = p.data_set_view_id 
+  GROUP BY 
+    res_id, 
+    role_id
+),
+
+res_role_perm_counts AS (
+  SELECT 
+    rp.res_id,
+    perm_level,
+    dsv_count_per_res_role,
+    dsv_count_per_res,
+    role_name,
+    is_public_role
+  FROM
+    res_role_perm rp
+  JOIN
+    role_base r ON rp.role_id = r.role_id
+  JOIN
+    res_dsv_total_count t ON rp.res_id = t.res_id
+),
+
+res_public_perm_counts AS (
+  SELECT
+    *
+  FROM
+    res_role_perm_counts rc
   WHERE
     is_public_role IS TRUE 
 ),
 
-dsv_private_perm AS (
-  SELECT 
+res_other_perm_counts AS (
+  SELECT
     *
-  FROM 
-    dsv_perm_base
-  WHERE
-    is_public_role IS FALSE  
-),
-
-dsv_perm AS ( -- Permissions einer Rolle für ein datasetview. Falls eine public PERMISSION besteht und diese höher ist, wird diese verwendet (übersteuerung)
-	SELECT 
-		sp.role_id,
-		data_set_view_id AS dsv_id,
-		CASE 
-		  WHEN COALESCE(perm_level, '0_non_public') > level_ THEN perm_level -- --> public permission level wins 
-		  ELSE level_
-		END AS perm_level
-	FROM 
-		simi.simiiam_permission sp 		
-	LEFT JOIN
-	 dsv_public_perm p ON sp.data_set_view_id = p.dsv_id 
-),
-
-fl_public_perm AS (
-  SELECT 
-    role_id,
-    facade_layer_id,
-    min(perm_level) AS level_public,
-    count(*) AS count_public
-  FROM 
-    simi.simiproduct_properties_in_facade pif
-  JOIN
-    dsv_public_perm p ON pif.data_set_view_id = p.dsv_id
-  GROUP BY 
-    facade_layer_id, role_id 
-),
-
-fl_private_perm AS (
-  SELECT 
-    role_id,
-    facade_layer_id,
-    min(perm_level) AS perm_level,
-    count(*) AS count_private
-  FROM 
-    simi.simiproduct_properties_in_facade pif
-  JOIN
-    dsv_private_perm p ON pif.data_set_view_id = p.dsv_id
-  GROUP BY 
-    facade_layer_id, role_id 
-),
-
-fl_all_children_count AS (
-  SELECT 
-    facade_layer_id,
-    count(*) AS count_all
-  FROM 
-    simi.simiproduct_properties_in_facade pif
-  GROUP BY 
-    facade_layer_id
-),
-
-/* Aus den Kindern abgeleitete Berechtigung einer Rolle für einen Facadelayer. 
- * 
- * Fl mit gemischten Berechtigungen public und 2-n private werden noch als berechtigt 
- * ausgegeben (was falsch ist). Dies wird in folgenden CTEs adressiert.
- */
-fl_perm_raw AS (
-  SELECT 
-    COALESCE(priv.facade_layer_id, pub.facade_layer_id) AS fl_id,
-    COALESCE(priv.role_id, pub.role_id) AS role_id,
-    CASE 
-      WHEN COALESCE(perm_level, '999_max_level') < COALESCE(level_public, '999_max_level') THEN COALESCE(perm_level, '999_max_level')
-      ELSE COALESCE(level_public, '999_max_level')
-    END AS perm_level, -- Falls sowohl private wie public vorhanden, "erbt" der fl den tiefsten level
-    count_private,
-    count_public  
   FROM
-    fl_public_perm pub
-  FULL OUTER JOIN
-    fl_private_perm priv ON pub.facade_layer_id = priv.facade_layer_id
+    res_role_perm_counts rc
+  WHERE
+    is_public_role IS FALSE 
 ),
 
-/* Aus den Kindern abgeleitete Berechtigung einer Rolle für einen Facadelayer. 
+/*
+ * Vereinigt die Eigenschaften der Gruppe bezüglich der entsprechenden 
+ * privaten Rolle und der Rolle "public".
+ * Aus den beiden Informationen wird berechnet,
+ * welche Berechtigung für die Rolle und DSV-Gruppe resultiert.
  * 
- * Mit der Where-Bedingung wird sichergestellt, dass Facadelayer mit teilweiser
- * Berechtigung für mehrere private Rollen nicht berechtigt werden.
- * Beispiel mit den privaten Rollen A und B, Public (P) und DatasetView 1-3:
+ * Mit der Where-Bedingung in der folgenden CTE wird sichergestellt, 
+ * dass Gruppen mit teilweiser Berechtigung für private Rollen nicht berechtigt werden.
+ * Beispiele mit den privaten Rollen A und B, Public (P) und DatasetView 1-3:
+ * 
  * 1 A
  * 2 B
  * 3 P
+ * --> Gruppe wird nicht berechtigt.
  * 
- * Der Facadelayer darf für keine der Rollen berechtigt werden. Im folgenen 
- * Beispiel darf der Facadelayer nur für B berechtigt sein:
  * 1 A,B
  * 2 B
  * 3 P
+ * --> Gruppe wird für B berechtigt.
+ * 
+ * 1 A
+ * 2 B,P (read-write für B, read für P)
+ * 3 P
+ * --> Gruppe wird für A berechtigt. 
  */
-fl_perm AS ( 
+role_perm_raw AS (
   SELECT
-    fl_id,
-    r.role_id,
-    perm_level
+    COALESCE(o.res_id, p.res_id) AS res_id,
+    COALESCE(o.role_name, p.role_name) AS role_name,
+    COALESCE(p.perm_level, o.perm_level) AS perm_level, -- Falls eine public-berechtigung vorliegt, hat diese vorrang
+    COALESCE(o.dsv_count_per_res, p.dsv_count_per_res) AS dsv_count_per_res,
+    COALESCE(o.dsv_count_per_res_role, 0) AS count_other_role,
+    COALESCE(p.dsv_count_per_res_role, 0) AS count_public_role
   FROM
-    fl_perm_raw r
-  JOIN
-    fl_all_children_count c ON r.fl_id = c.facade_layer_id
-  WHERE
-    count_all = COALESCE(count_public, 0) + COALESCE(count_private, 0) 
+    res_other_perm_counts o
+  FULL OUTER JOIN    
+    res_public_perm_counts p ON o.res_id = p.res_id
 ),
 
-sa_perm_union AS ( -- permissions for single actors
-	SELECT role_id, dsv_id AS sa_id, perm_level FROM dsv_perm
-	UNION ALL 
-	SELECT role_id, fl_id AS sa_id, perm_level FROM fl_perm
+res_ident AS (
+  SELECT identifier AS res_ident, id FROM simi.simiproduct_data_product 
+  UNION ALL
+  SELECT "name" AS res_ident, id FROM simi.simi.simiextended_dependency 
 ),
 
-sa_perm AS (
+role_perm AS (
   SELECT
-    sa_id,  
-    role_id,
-    perm_level,
-    (r."name" = 'public') AS is_public_role
-  FROM 
-    sa_perm_union sa
-  JOIN
-    simi.simiiam_role r ON sa.role_id = r.id  
-),
-
-layergroup_with_public_sa AS ( -- Alle Produktlisten mit 1-n "public" Kindern
-  SELECT 
-    lg.id AS lg_id,
-    sa.role_id,
-    min(sa.perm_level) AS level_public,
-    count(*) AS count_public
-  FROM
-    simi.simiproduct_properties_in_list p
-  JOIN
-    sa_perm sa ON p.single_actor_id = sa.sa_id
-  JOIN
-    simi.simiproduct_layer_group lg ON p.product_list_id = lg.id
-  WHERE
-    sa.is_public_role IS TRUE 
-  GROUP BY 
-    lg.id, sa.role_id 
-),
-
-layergroup_with_private_sa AS ( -- Alle Layergruppen mit 1-n "private" Kindern
-  SELECT 
-    lg.id AS lg_id,
-    sa.role_id,
-    min(sa.perm_level) AS level_private,
-    count(*) AS count_private
-  FROM
-    simi.simiproduct_properties_in_list p
-  JOIN
-    sa_perm sa ON p.single_actor_id = sa.sa_id
-  JOIN
-    simi.simiproduct_layer_group lg ON p.product_list_id = lg.id
-  WHERE
-    sa.is_public_role IS FALSE  
-  GROUP BY 
-    lg.id, sa.role_id 
-),
-
-pl_all_children_count AS (
-  SELECT 
-    product_list_id AS pl_id,
-    count(*) AS count_all
-  FROM
-    simi.simiproduct_properties_in_list
-  GROUP BY 
-    product_list_id
-),
-
-lg_perm_raw AS ( -- Gleiche Logik wie bei fl_perm_raw
-  SELECT 
-    COALESCE(priv.lg_id, pub.lg_id) AS lg_id,
-    COALESCE(priv.role_id, pub.role_id) AS role_id,
-    CASE 
-      WHEN COALESCE(level_private, '999_max_level') < COALESCE(level_public, '999_max_level') THEN COALESCE(level_private, '999_max_level')
-      ELSE COALESCE(level_public, '999_max_level')
-    END AS perm_level, -- Falls sowohl private wie public vorhanden, "erbt" die lg den tiefsten level
-    count_public,
-    count_private
-  FROM
-    layergroup_with_public_sa pub
-  FULL OUTER JOIN
-    layergroup_with_private_sa priv ON pub.lg_id = priv.lg_id
-),
-
-lg_perm AS ( 
-  SELECT
-    r.lg_id,
-    r.role_id,
-    perm_level,
-    count_all,
-    count_public,
-    count_private
-  FROM
-    lg_perm_raw r
-  JOIN
-    pl_all_children_count c ON r.lg_id = c.pl_id
-  WHERE
-    count_all = COALESCE(count_public, 0) + COALESCE(count_private, 0) 
-),
-
-dp_perm_union AS ( -- permissions for the dataproducts datasetview, facadelayer and layergroup
-	SELECT role_id, sa_id AS dp_id, perm_level FROM sa_perm
-	UNION ALL 
-	SELECT role_id, lg_id AS dp_id, perm_level FROM lg_perm
-),
-
-dp_perm AS ( -- lists all permissions for dataproducts in "human readable" form
-	SELECT 
-		role_id,
-		dp_id,
-		identifier AS res_ident,
-		CASE perm_level
-			WHEN '2_read_write'
-				THEN true
-			ELSE false
-		END AS writeable
-	FROM 
-		dp_perm_union p
-	JOIN 
-		simi.simiproduct_data_product dp ON p.dp_id = dp.id		
-),
-
-dep_dsv_count_total AS (
-  SELECT 
-    dependency_id AS dep_id,
-    count(*) AS dsv_count_total
-  FROM 
-    simi.simiextended_relation 
-  WHERE 
-    relation_type = '2_data'
-  GROUP BY
-    dependency_id
-),
-
-dep_permission_base AS (
-  SELECT 
-    dependency_id AS dep_id,
-    p.role_id, 
-    count(*) AS dsv_count_for_role,
-    min(level_) AS perm_level
-  FROM 
-    simi.simiextended_relation r
-  JOIN
-    simi.simiiam_permission p ON r.data_set_view_id = p.data_set_view_id 
-  WHERE 
-    r.relation_type = '2_data'
-  GROUP BY
-    dependency_id, role_id
-),
-
-/* Aus den DSV abgeleitete Berechtigung einer Rolle für eine Dependency (Report, ...). 
- * 
- * Mit der Where-Bedingung wird sichergestellt, dass Dependencies mit teilweiser
- * Berechtigung für mehrere private Rollen nicht berechtigt werden.
- * Beispiel mit den privaten Rollen A und B, Public (P) und DatasetView 1-3:
- * 
- * Im Gegensatz zu den Dataproducts ist public nicht als Spezialrolle berücksichtigt.
- * Darum muss auch für von einem Report referenzierte "public dsv" 
- * die Rolle des Report-Aufrufers konfiguriert werden, damit der Report für die 
- * Rolle berechtigt ist. 
- */
-dep_perm AS (
-  SELECT 
-    role_id,
-    d.name AS res_ident,
-    FALSE AS writeable,
-    perm_level,
-    d.dtype AS dep_type
-  FROM 
-    simi.simiextended_dependency d
-  JOIN
-    dep_permission_base p ON d.id = p.dep_id
-  JOIN
-    dep_dsv_count_total c ON d.id = c.dep_id
-  WHERE 
-      d.dtype IN ('simiExtended_Report', 'simiExtended_FeatureInfo')
-    AND 
-      dsv_count_total = dsv_count_for_role
-),
-
-allperm_raw AS (
-  SELECT role_id, res_ident, writeable FROM dp_perm
-  UNION ALL 
-  SELECT role_id, res_ident, writeable FROM dep_perm
-),
-
-perm_json AS (
-  SELECT 
-    role_id,
-    jsonb_object_agg(
-      res_ident,
-      jsonb_build_object('writable', writeable)
-    ) AS perm_json
-  FROM 
-    allperm_raw
-  GROUP BY
-    role_id 
-)
-
-SELECT 
-  jsonb_set( -- Inner jsonb_set(...) sets the perm_json array. Outer jsonb_set(...) sets the role name
-    jsonb_set('{"role": null, "permissions": { "all_services": null }}', '{permissions, all_services}', perm_json),
-    '{role}',
-    to_jsonb(name))
-  AS js
-FROM 
-  perm_json p
-JOIN 
-  simi.simiiam_role r ON p.role_id = r.id
-
-
-/* DEBUG *************************************************************************************************************
- * 
- * Es folgen CTEs, mit denen die Informationen zu den "Schlüssel-CTEs" dsv_perm, fl_perm, lg_perm, rep_perm 
- * ausgegeben werden.
-
-,
-
-debug_dp_union AS (
-  SELECT 'dsv_perm' AS cte, role_id, dsv_id AS res_id, perm_level FROM dsv_perm
-  UNION ALL 
-  SELECT 'fl_perm' AS cte, role_id, fl_id AS res_id, perm_level FROM fl_perm
-  UNION ALL 
-  SELECT 'lg_perm' AS cte, role_id, lg_id AS res_id, perm_level FROM lg_perm
-),
-
-debug_dp AS ( 
-  SELECT
-    u.*,
-    identifier AS res_ident
-  FROM
-    debug_dp_union u
-  JOIN
-    simi.simiproduct_data_product p ON u.res_id = p.id
-  WHERE 
-    identifier LIKE 'test.perm.%'
-),
-
-debug_union AS (
-  SELECT res_ident, role_id, cte, perm_level FROM debug_dp
-  UNION ALL 
-  SELECT res_ident, role_id, 'dep_perm' AS cte, perm_level FROM dep_perm
-),
-
-debug AS (
-  SELECT
-    res_ident,
-    r.name AS role_name,
-    cte,
-    perm_level
-  FROM
-    debug_union d
-  JOIN
-    simi.simiiam_role r ON d.role_id = r.id
-  ORDER BY 
     res_ident,
     role_name,
-    cte
+    perm_level,
+    CASE
+      WHEN perm_level = '2_read_write' THEN jsonb_build_object('writable', TRUE)
+      ELSE jsonb_build_object()
+    END AS writable_json,
+    dsv_count_per_res AS tot,
+    count_other_role AS other,
+    count_public_role AS public
+  FROM
+    role_perm_raw rp
+  JOIN
+    res_ident res ON rp.res_id = res.id
+  WHERE
+    (count_other_role + count_public_role) >= dsv_count_per_res
+),
+
+role__res_obj AS (
+  SELECT
+    role_name,
+    jsonb_object_agg(res_ident, writable_json) AS res_obj
+  FROM
+    role_perm
+  GROUP BY
+    role_name
 )
 
-SELECT * FROM debug
-*/
-
-
-
-
-
-
-	
+SELECT
+  jsonb_build_object(
+    'role', role_name, 
+    'permissions', jsonb_build_object('all_services', res_obj)
+  ) AS role_obj
+FROM 
+  role__res_obj

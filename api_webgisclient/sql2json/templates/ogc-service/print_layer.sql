4ae1c095c56f4b34b27d78c27f416517fdc8c16f
uncoyote <jeker.git@outlook.de>
2021-08-23 08:23:39 +0200

    print: conf _sw, _ortho

--- api_webgisclient/sql2json/templates/ogc-service/print_layer.sql
+++ api_webgisclient/sql2json/templates/ogc-service/print_layer.sql
@@ -3,4 +3,6 @@ SELECT
 FROM 
   simi.trafo_wms_layer_v
 WHERE 
-  print_or_ext IS TRUE 
+    print_or_ext IS TRUE 
+  OR 
+    identifier IN ('ch.so.agi.hintergrundkarte_sw', 'ch.so.agi.hintergrundkarte_farbig', 'ch.so.agi.hintergrundkarte_ortho')

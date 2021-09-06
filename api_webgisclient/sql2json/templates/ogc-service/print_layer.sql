0f7e49faac11558bcf6a9f2236867b32302c3f53
uncoyote <jeker.git@outlook.de>
2021-09-06 07:53:37 +0200

    ogc: removed or for print layers in whereclause

--- api_webgisclient/sql2json/templates/ogc-service/print_layer.sql
+++ api_webgisclient/sql2json/templates/ogc-service/print_layer.sql
@@ -4,5 +4,3 @@ FROM
   simi.trafo_wms_layer_v
 WHERE 
     print_or_ext IS TRUE 
-  OR 
-    identifier IN ('ch.so.agi.hintergrundkarte_sw', 'ch.so.agi.hintergrundkarte_farbig', 'ch.so.agi.hintergrundkarte_ortho')

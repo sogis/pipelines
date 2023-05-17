030b280ba5e8a8d9b8214648cc7ed9ee2ccabcc4
pfeimich <michael.pfeiffer@bd.so.ch>
2023-05-17 13:12:13 +0200

    [qgis-server] change name for test form

--- api_webgisclient/qwc-service/sql2json/foreground_map.sql
+++ api_webgisclient/qwc-service/sql2json/foreground_map.sql
@@ -176,7 +176,7 @@ edit_layers AS (
       identifier, 
       jsonb_build_object(
         'editDataset', identifier,
-	'form', ':/forms/autogen/somap_ch.so.agi.sgv.meldungen.ui?v=1678288732',
+	'form', ':/forms/autogen/somap_ch.so.awjf.waldpflege_erfassung.ui?v=1678288732',
         'layerName', dp.title,
         'fields', attr_arr,
         'geomType', initcap(t.geo_type) 

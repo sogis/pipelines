0c69f1291299c73ced77b0a416b74a8ccf5bc10f
pfeimich <michael.pfeiffer@bd.so.ch>
2023-06-19 19:10:04 +0200

    [permission] add empty value for attributes if type raster view

--- api_webgisclient/permission/sql2json/dataprod_properties.sql
+++ api_webgisclient/permission/sql2json/dataprod_properties.sql
@@ -2,7 +2,7 @@ WITH
 
 tab_layer AS ( 
 	SELECT 
-		jsonb_build_object('name', derived_identifier , 'attributes', attr_names_json) AS js
+		jsonb_build_object('name', derived_identifier , 'attributes', COALESCE(attr_names_json,'[]')) AS js
 	FROM 
 		simi.simiproduct_data_product dp
 	LEFT JOIN

33350cb80d2d4113623665fe96cf1b1f51171324
pfeimich <michael.pfeiffer@bd.so.ch>
2023-06-21 16:36:06 +0200

    [permission] test for using info_templates for raster views

--- api_webgisclient/permission/sql2json/role_permissions.sql
+++ api_webgisclient/permission/sql2json/role_permissions.sql
@@ -6,6 +6,25 @@ res_ident AS (
   SELECT split_part("name", '.', 1) AS res_ident, id FROM simi.simi.simiextended_dependency -- split_part(..) zur entfernung des format-suffix im namen (.xlsx in av_gb_uebersicht.xlsx)
 ),
 
+dsv_dependency_unique AS (
+SELECT 
+    data_set_view_id,
+    d.dtype AS dependency_type,
+    (max(dependency_id::varchar))::uuid AS dependency_id
+  FROM
+    simi.simiextended_relation r
+  JOIN
+    simi.simiextended_dependency d ON r.dependency_id = d.id
+  WHERE
+    r.relation_type = '1_display'
+  AND 
+	d.dtype = 'simiExtended_FeatureInfo'
+  GROUP BY
+    data_set_view_id,
+    d.dtype
+	order by data_set_view_id
+),
+
 role_perm AS (
   SELECT
     res_ident,
@@ -14,7 +33,11 @@ role_perm AS (
     CASE
       WHEN perm_level = '2_read_write' THEN jsonb_build_object('writable', TRUE)
       ELSE jsonb_build_object()
-    END AS writable_json
+    END AS writable_json,
+    CASE
+      WHEN dependency_type = 'simiExtended_FeatureInfo' THEN jsonb_build_object('info_template', TRUE)
+      ELSE jsonb_build_object()
+    END AS info_template_json
   FROM
     simi.trafo_resource_role_v rp
   JOIN
@@ -24,7 +47,7 @@ role_perm AS (
 role__res_obj AS (
   SELECT
     role_name,
-    jsonb_object_agg(res_ident, writable_json) AS res_obj
+    jsonb_object_agg(res_ident, writable_json || info_template_json) AS res_obj
   FROM
     role_perm
   GROUP BY

c39d1220361b67ee315a8cd5be8edc0b65281cb0
pfeimich <michael.pfeiffer@bd.so.ch>
2023-09-26 15:42:33 +0200

    [qwc-service] test form path without map/assets

--- api_webgisclient/qwc-service/sql2json/foreground_map.sql
+++ api_webgisclient/qwc-service/sql2json/foreground_map.sql
@@ -187,7 +187,7 @@ edit_layers AS (
         'layerName', dp.title,
         'fields', attr_arr,
         'geomType', initcap(t.geo_type),
-	'form', '/map/assets/forms/autogen/somap_'::character varying || identifier || '.ui'::character varying,
+	'form', '/forms/autogen/somap_'::character varying || identifier || '.ui'::character varying,
 	'permissions', w.permissions
       )
     ) AS edit_keyval
